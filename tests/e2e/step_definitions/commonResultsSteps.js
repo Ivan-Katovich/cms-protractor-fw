d526f00f55427a06e355a1879fc9a3a14faf4ed5
Ivan Katovich <Ivan_Katovich@epam.com>
2015-09-02 18:01:31 +0300

    extend ElementArrayFinder and create waitForResults

--- tests/e2e/step_definitions/commonResultsSteps.js
+++ tests/e2e/step_definitions/commonResultsSteps.js
@@ -3,6 +3,15 @@ var browserUtils = require('./../support/helpers/browserUtils');
 
 var steps = function(){
 
+    this.When(/^I wait for all results load$/, function (callback) {
+        browser.ignoreSynchronization = true;
+        pageFactory.currentPage.waitForResults()
+            .then(function(){
+                browser.ignoreSynchronization = false;
+            })
+            .then(callback);
+    });
+
     this.Then(/^results page main title should be visible$/, function (callback) {
         pageFactory.currentPage.isMainTitleVisible()
             .then(function(isVisible){
